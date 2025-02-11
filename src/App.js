cc09ac8da74a66114ddc977fa74e6b2e70a2c545
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-02-11 08:17:01 +0300

    tasks 7

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+	return <div>
+		text
+		<h1>
+		Язык JSX
+		</h1>
+		<p>
+		Язык JSX - это обычный JavaScript, но с некоторыми дополнениями, позволяющими писать теги прямо в коде, без кавычек.
+		</p>
+		
+	</div>;
+	
+}
+
+
+export default App;
