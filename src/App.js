2e06b46d61208514a59ac65b340e255a54d0ab0c
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-02-13 17:43:06 +0300

    10 tasks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React from 'react';
 
 function App() {
-	return <div>
+	return ( 
+	<>
+		<div>
 		Vilen
-		<h1>
-		Язык JSX
-		</h1>
-		<p calss = 'eee'>
-		Язык JSX - это обычный JavaScript, но с некоторыми дополнениями, позволяющими писать теги прямо в коде, без кавычек.
-		</p>
+			<h1>
+				Язык JSX
+			</h1>
+			<p calss = 'eee'>
+				Язык JSX - это обычный JavaScript, но с некоторыми дополнениями, позволяющими писать теги прямо в коде, без кавычек.
+			</p>
+			<ul>
+				<li>Vilen</li>
+				<li>Dasha</li>
+				<li>Melisa</li>
+				<li>Elnur</li>
+				<li>Achmet</li>
+				<li>Edem</li>
+				<li>Aider</li>
+				<li>Ervin</li>
+			</ul>
+		</div>
 		<ul>
-			<li>Vilen</li>
-			<li>Dasha</li>
-			<li>Melisa</li>
-			<li>Elnur</li>
-			<li>Achmet</li>
-			<li>Edem</li>
-			<li>Aider</li>
-			<li>Ervin</li>
+			<li>text1</li>
+			<li>text2</li>
+			<li>text3</li>
 		</ul>
-	</div>;
+		<ul>
+			<li>text1</li>
+			<li>text2</li>
+			<li>text3</li>
+		</ul>;
+	</>
 	
-	
+		
+	);
 }
 
 
