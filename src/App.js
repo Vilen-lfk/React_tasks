160c215bbf0cd7531f71e5ccdeb4bd898b216e8d
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-02-13 17:33:08 +0300

    8 tasks

--- src/App.js
+++ src/App.js
@@ -9,8 +9,18 @@ function App() {
 		<p calss = 'eee'>
 		Язык JSX - это обычный JavaScript, но с некоторыми дополнениями, позволяющими писать теги прямо в коде, без кавычек.
 		</p>
-		
+		<ul>
+			<li>Vilen</li>
+			<li>Dasha</li>
+			<li>Melisa</li>
+			<li>Elnur</li>
+			<li>Achmet</li>
+			<li>Edem</li>
+			<li>Aider</li>
+			<li>Ervin</li>
+		</ul>
 	</div>;
+	
 	
 }
 
