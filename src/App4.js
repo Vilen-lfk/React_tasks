6f395ba426e71ba8254abe12f7382aeedb2c9d18
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-03-05 13:00:05 +0300

    75-76 tasks

--- src/App4.js
+++ src/App4.js
@@ -15,6 +15,38 @@ function App() {
 			{note}
 		</li>;
 	});
+	//75
+	const initNotes = [
+		{
+			id: 'GYi9G_uC4gBF1e2SixDvu',
+			prop1: 'value11',
+			prop2: 'value12',
+			prop3: 'value13',
+		},
+		{
+			id: 'IWSpfBPSV3SXgRF87uO74',
+			prop1: 'value21',
+			prop2: 'value22',
+			prop3: 'value23',
+		},
+		{
+			id: 'JAmjRlfQT8rLTm5tG2m1L',
+			prop1: 'value31',
+			prop2: 'value32',
+			prop3: 'value33',
+		},
+	];
+	const [vils, setVils] = useState(initNotes);
+	function addVils(){
+		setVils([...vils, vils.length + 1]);
+	}
+	const res = vils.map(vil => {
+		return <li key={vil.id}>
+			<span>{vil.prop1}</span>
+			<span>{vil.prop2}</span>
+			<span>{vil.prop3}</span>
+		</li>;
+	});
 	return (
 	<>
 		<div>
@@ -22,6 +54,13 @@ function App() {
 			
 			<ul>
 				{result}
+			</ul>
+		</div>
+		<div>
+			<h1>75 Задание</h1>
+			<ul>
+				{res}
+				<button onClick={addVils}>Добавить элемент</button>
 			</ul>
 		</div>
 	</>
