d661dfbb70b031e8aead2b9e9106bd36a65c889e
Vilen-lfk <karamurzayev.amet@mail.ru>
2025-03-10 11:49:45 +0300

    86-91 tasks

--- src/App6.js
+++ src/App6.js
@@ -2,18 +2,51 @@ import React, { useState } from 'react';
 import uuid from "react-uuid";
 import State from "./State";
 
-function App(props) {
+function App() {
     //85
 	const initUsers = [
-        {id: uuid(1), name: 'Aider', surname: 'surn1', age: 30},
-        {id: uuid(2), name: 'Edem', surname: 'surn2', age: 31},
-        {id: uuid(3), name: 'Alim', surname: 'surn3', age: 32},
+        {id: uuid(1), name: 'Aider', surname: 'surn1', age: 30, inCart: false},
+        {id: uuid(2), name: 'Edem', surname: 'surn2', age: 31, inCart: false},
+        {id: uuid(3), name: 'Alim', surname: 'surn3', age: 32, inCart: false},
     ];
     const [users, setUsers] = useState(initUsers)
+    
 	const renderUsers = users.map(user => (
-        <State key={user.id} name={user.name} surname={user.surname} age={user.age} />
+        <State key={user.id} э
+        name={user.name} 
+        surname={user.surname} 
+        age={user.age} 
+        inCart={user.inCart}
+        addToCart={addToCart}/>
     ));
-    //86
+    //86-87-88
+    
+    function addToCart(id) {
+        setUsers(users.map(user => {
+            if (user.id === id) {
+                return { ...user, inCart: true };  // Создаем новый объект с обновленным значением inCart
+            }
+            return user;
+        }));
+    }
+    function toggleMode(id) {
+        setUsers(users.map(use => {
+            if (use.id === id) {
+                use.isEdit = !use.isEdit;
+            }
+            
+            return use;
+        }));
+    }
+    function editProd(id, field, event) {
+        setUsers(users.map(use => {
+            if (use.id === id) {
+                use[field] = event.target.value;
+            }
+            
+            return use;
+        }));
+    }
     const items = users.map(use =>{
         return <State
         key = {use.id}
@@ -21,16 +54,20 @@ function App(props) {
         name ={use.name}
         surname = {use.surname}
         age = {use.age}
+        isEdit={use.isEdit}
+		toggleMode={toggleMode}
+		editProd={editProd}
         />
     })
+    
     return (
         <>
            <div>
             <h1>85 Задача</h1>
-           {renderUsers}
+            {/*{renderUsers}*/}
 		</div>
         <div>
-            <h1>86 Задача</h1>
+            <h1>86-87 Задача</h1>
             {items}    
         </div> 
         </>
--- src/State.js
+++ src/State.js
@@ -1,16 +1,41 @@
 import React from 'react';
 
-function State(props) {
-    return (
-        <>
-            <p>
-                id: <span>{props.id}</span> <br />
-                name: <span>{props.name}</span> <br />
-                surname: <span>{props.surname}</span> <br />
-                age: <span>{props.age}</span> <br />
-            </p>
-        </>
-    );
+function State({ id, name, surname, age,inCart ,addToCart,isEdit, toggleMode, editProd }) {
+    function ProductField({ id, text, 
+        type, isEdit, editProd }) { 
+        return isEdit
+            ? <input value={text} onChange={event => editProd(id, type, event)} /> 
+            : <span>{text}</span>
+        ;
+    }
+    return <div>
+       
+        name: <ProductField
+			id={id}
+			text={name}
+			type="name"
+			isEdit={isEdit}
+			editProd={editProd}
+		/>,
+        surname: <ProductField
+			id={id}
+			text={surname}
+			type="surname"
+			isEdit={isEdit}
+			editProd={editProd}
+		/>,
+        age: <ProductField
+			id={id}
+			text={age}
+			type="age"
+			isEdit={isEdit}
+			editProd={editProd}
+		/>,
+        
+        <button onClick={() => toggleMode(id)}>
+			{isEdit ? 'save': 'edit'}
+		</button>
+    </div>;
 }
 
 export default State;
